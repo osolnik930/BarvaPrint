676974153e2bb96649e1e9d0fc8d01ea9ad4c48b
Osolnik <nejc.osolnik@gmail.com>
2026-04-13 21:36:20 +0200

    par sprememb za boljso berljivost kode

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-barva = input("Napišite svojo najljubšo barvo: ")
-print(f"Barva {barva} je pa res lepa barva!")
+# input iz cmdja
+barva_1 = input("Napišite svojo najljubšo barvo: ")
+
+# print barve
+print(f"Barva {barva_1} je pa res lepa barva!")
